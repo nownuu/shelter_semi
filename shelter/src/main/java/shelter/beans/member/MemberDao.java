ca7823ffdaacc68e4d06c4fef4b366cc3cb40a3a
nownuu98 <nownuu98@naver.com>
2023-11-27 20:44:21 +0900

    20231127 - 아이디 찾기, 비밀번호 찾기, 정보 수정, 정보 보기 dao 구현
    내일 -> servlet / test 내 jsp 만들어보기

--- shelter/src/main/java/shelter/beans/member/MemberDao.java
+++ shelter/src/main/java/shelter/beans/member/MemberDao.java
@@ -73,12 +73,112 @@ public class MemberDao {
     }
 
     // 아이디 찾기
-    
+    public String findId(String memberName, String memberPhone) {
+        String sql = "SELECT member_id FROM member WHERE member_name=? AND member_phone=?";
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, memberName);
+            ps.setString(2, memberPhone);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getString("member_id");
+                }
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            JdbcUtil.close(this.conn, null, null);
+        }
+
+        return null;
+    }
+
     // 비밀번호 찾기
-    
-    // 정보 수정
+    public String findPw(String memberId, String memberName, String memberPhone) {
+    	String sql = "SELECT member_pw FROM member WHERE member_id=? AND member_name=? AND member_phone=?";
+    	
+    	try(PreparedStatement ps = conn.prepareStatement(sql)){
+    		
+    		ps.setString(1, memberId);
+    		ps.setString(2, memberName);
+            ps.setString(3, memberPhone);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getString("member_pw");
+                }
+            }
+    		
+    	} catch(SQLException ex) {
+    		ex.printStackTrace();
+    	} finally {
+    		JdbcUtil.close(this.conn, null, null);
+    	}
+    	return null;
+    }
     
     // 정보 보기
+    public MemberDto getMemberById(String memberId) {
+        String sql = "SELECT * FROM member WHERE member_id=?";
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, memberId);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    MemberDto memberDto = new MemberDto();
+
+                    memberDto.setMemberId(rs.getString("member_id"));
+                    memberDto.setMemberPw(rs.getString("member_pw"));
+                    memberDto.setMemberPhone(rs.getString("member_phone"));
+                    memberDto.setMemberEmail(rs.getString("member_email"));
+                    memberDto.setMemberGender(rs.getString("member_gender"));
+                    memberDto.setMemberAddress(rs.getString("member_address"));
+
+                    return memberDto;
+                }
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            JdbcUtil.close(this.conn, null, null);
+        }
+        return null;
+    }
+
+    
+    // 정보 수정
+    public void editMember(MemberDto member) {
+        String sql = "UPDATE member SET member_pw=?, member_phone=?, member_email=?, member_gender=?, member_address=? WHERE member_id=?";
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, member.getMemberPw());
+            ps.setString(2, member.getMemberPhone());
+            ps.setString(3, member.getMemberEmail());
+            ps.setString(4, member.getMemberGender());
+            ps.setString(5, member.getMemberAddress());
+            ps.setString(6, member.getMemberId());
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Member information updated successfully");
+            } else {
+                System.out.println("No member found with the specified ID");
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            JdbcUtil.close(this.conn, null, null);
+        }
+    }
+
     
     //  회원 탈퇴
+    
+    
 }
