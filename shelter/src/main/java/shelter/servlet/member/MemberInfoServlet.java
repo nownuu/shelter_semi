ef91ed0c8c3c02b62a55049e1ae2a18d9e0300cc
nownuu98 <nownuu98@naver.com>
2023-11-30 11:37:12 +0900

    20231130 - 회원 정보 수정, 회원 탈퇴 모듈

---
+++ shelter/src/main/java/shelter/servlet/member/MemberInfoServlet.java
@@ -0,0 +1,40 @@
+package shelter.servlet.member;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import shelter.beans.member.MemberDao;
+import shelter.beans.member.MemberDto;
+
+@WebServlet("/test/memberInfo.do")
+public class MemberInfoServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String memberId = (String) req.getSession().getAttribute("uid");
+            
+            // 콘솔 확인용
+            System.out.println("Member ID from session: " + memberId);
+
+            if (memberId != null) {
+                MemberDao memberDao = new MemberDao();
+                MemberDto memberDto = memberDao.getMemberById(memberId);
+                
+                // 콘솔 확인용
+                System.out.println("인포 서블릿"+memberDto);
+                
+                req.setAttribute("memberDto", memberDto);
+                req.getRequestDispatcher("/test/memberInfo.jsp").forward(req, resp);
+            } else {
+                resp.sendRedirect(req.getContextPath() + "/test/login.jsp");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ shelter/src/main/webapp/test/memberQuit.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Withdraw Member</title>
+</head>
+<body>
+    <h2>Withdraw Member</h2>
+    
+    <p>정말로 탈퇴하시겠습니까?</p>
+    
+    <form action="memberQuit.do" method="post">
+        <input type="submit" value="Withdraw">
+    </form>
+</body>
+</html>
